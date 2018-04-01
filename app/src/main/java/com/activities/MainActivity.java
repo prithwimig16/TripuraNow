fbc814f136c3419780f0a00f35f5cc8e077d779a
Prithwiraj Nath <prithwiraj.nath@purpleyo.com>
2018-04-02 02:49:49 +0530

    End of Day1

--- app/src/main/java/com/activities/MainActivity.java
+++ app/src/main/java/com/activities/MainActivity.java
@@ -1,8 +1,10 @@
 package com.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,27 +15,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.adapters.TabsFragmentAdapter;
 import com.tripuranow.R;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import java.util.ArrayList;
 
+import devlight.io.library.ntb.NavigationTabBar;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    TabsFragmentAdapter tabsFragmentAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -42,6 +38,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        initUI();
     }
 
     @Override
@@ -79,25 +76,143 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-//        int id = item.getItemId();
-//
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
+         //Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_news) {
+            // Handle the camera action
+        } else if (id == R.id.nav_buzz) {
+
+        } else if (id == R.id.nav_live) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    private void initUI() {
+
+        //final ViewPager viewPager=(ViewPager)findViewById(R.id.tab_bar_pager);
+       // this.tabsFragmentAdapter = new TabsFragmentAdapter(getSupportFragmentManager());
+        //viewPager.setAdapter(this.tabsFragmentAdapter);
+
+
+
+        final String[] colors = getResources().getStringArray(R.array.tab_bar_item_colors);
+
+        final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb_horizontal);
+        final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_menu_camera),
+                        Color.TRANSPARENT)
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_menu_camera))
+                        .title("Services")
+//                        .badgeTitle("NTB")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_menu_manage),
+                        Color.TRANSPARENT)
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_menu_manage))
+                        .title("Upload")
+//                        .badgeTitle("with")
+                        .build()
+        );
+
+
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_menu_gallery),
+                        Color.TRANSPARENT)
+                        .selectedIcon(getResources().getDrawable(R.drawable.ic_menu_gallery))
+                        .title("Properties")
+                       // .isLargeIcon(true)
+//                        .badgeTitle("icon")
+                        .build()
+
+        );
+
+
+        navigationTabBar.setModels(models);
+        //navigationTabBar.setViewPager(viewPager, 0);
+        navigationTabBar.setBgColor (getResources().getColor( R.color.tabbarbgcolor));
+        navigationTabBar.setInactiveColor(getResources().getColor(R.color.tabbardefaultitemcolor));
+        navigationTabBar.setActiveColor(getResources().getColor(R.color.white));
+
+
+        navigationTabBar.setBehaviorEnabled(false);
+
+        navigationTabBar.setIsSwiped(false);
+
+
+        navigationTabBar.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
+
+//                Utils.consoleLog(MainTabActivity.class,"onPageScrolled");
+
+
+            }
+
+            @Override
+            public void onPageSelected(final int position) {
+                navigationTabBar.getModels().get(position).hideBadge();
+//                Utils.consoleLog(MainTabActivity.class,"onPageSelected");
+//
+//                Config.getSharedInstance().selectedTab = position;
+//                switch (position)
+//                {
+//                    case 0:
+//                        DashboardFragment dashboardFragment = (DashboardFragment)tabsFragmentAdapter.getItem(0);
+//                        dashboardFragment.refreshFragment();
+//                        break;
+//                    case 1:
+//                        LeadsFragment leadsFragment = (LeadsFragment) tabsFragmentAdapter.getItem(1);
+//                        leadsFragment.refreshFragment();
+//                        break;
+//
+//                    case 2:
+//                        RevenueFragment revenueFragment = (RevenueFragment) tabsFragmentAdapter.getItem(2);
+//                        revenueFragment.refreshFragment();
+//                        break;
+//
+//
+//                    case 3:
+//                        TeamActivitiesFragment teamActivitiesFragment = (TeamActivitiesFragment) tabsFragmentAdapter.getItem(3);
+////                        teamActivitiesFragment.refreshFragment();
+//                        break;
+//
+//
+//
+//                    case 4:
+//                        SettingsFragment settingsFragment = (SettingsFragment) tabsFragmentAdapter.getItem(4);
+//                        settingsFragment.refreshFragment();
+//                        break;
+//
+//
+//                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(final int state) {
+//                Utils.consoleLog(MainTabActivity.class,"onPageScrollStateChanged");
+
+
+            }
+        });
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/adapters/TabsFragmentAdapter.java
@@ -0,0 +1,11 @@
+package com.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+/**
+ * Created by sanniv on 7/26/17.
+ */
+
+public class TabsFragmentAdapter {
+}
