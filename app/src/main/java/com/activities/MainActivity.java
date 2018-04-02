617841e37ee7ef869ed1b15f8720f1aa92ab5ef2
Prithwiraj Nath <prithwiraj.nath@purpleyo.com>
2018-04-03 00:48:40 +0530

    End of Day1

--- app/src/main/java/com/activities/MainActivity.java
+++ app/src/main/java/com/activities/MainActivity.java
@@ -1,9 +1,14 @@
 package com.activities;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -16,18 +21,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.adapters.TabsFragmentAdapter;
+import com.fragments.BuzzFragment;
+import com.fragments.LiveFragment;
+import com.fragments.NewsFragment;
 import com.tripuranow.R;
 
 import java.util.ArrayList;
 
-import devlight.io.library.ntb.NavigationTabBar;
+
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     TabsFragmentAdapter tabsFragmentAdapter;
+    final String NEWS="News";
+    private Fragment currFragment;
+    BottomNavigationView bottomNavigationView;
+    String currTag ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -38,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        initUI();
+
     }
 
     @Override
@@ -51,45 +64,61 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
          //Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        Fragment selectedFragment = null;
         if (id == R.id.nav_news) {
-            // Handle the camera action
+
+            selectedFragment = new NewsFragment();
+            currTag = "news";
         } else if (id == R.id.nav_buzz) {
+            selectedFragment = new BuzzFragment();
+            currTag = "buzz";
 
         } else if (id == R.id.nav_live) {
+            selectedFragment = new LiveFragment();
+            currTag = "live";
 
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
         }
+
+        currFragment = selectedFragment;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.content_frame, currFragment,currTag);
+                transaction.commit();
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
@@ -99,119 +128,57 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private void initUI() {
 
-        //final ViewPager viewPager=(ViewPager)findViewById(R.id.tab_bar_pager);
-       // this.tabsFragmentAdapter = new TabsFragmentAdapter(getSupportFragmentManager());
-        //viewPager.setAdapter(this.tabsFragmentAdapter);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_menu_live);
 
-
-
-        final String[] colors = getResources().getStringArray(R.array.tab_bar_item_colors);
-
-        final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb_horizontal);
-        final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        getResources().getDrawable(R.drawable.ic_menu_camera),
-                        Color.TRANSPARENT)
-                        .selectedIcon(getResources().getDrawable(R.drawable.ic_menu_camera))
-                        .title("Services")
-//                        .badgeTitle("NTB")
-                        .build()
-        );
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        getResources().getDrawable(R.drawable.ic_menu_manage),
-                        Color.TRANSPARENT)
-                        .selectedIcon(getResources().getDrawable(R.drawable.ic_menu_manage))
-                        .title("Upload")
-//                        .badgeTitle("with")
-                        .build()
-        );
-
-
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        getResources().getDrawable(R.drawable.ic_menu_gallery),
-                        Color.TRANSPARENT)
-                        .selectedIcon(getResources().getDrawable(R.drawable.ic_menu_gallery))
-                        .title("Properties")
-                       // .isLargeIcon(true)
-//                        .badgeTitle("icon")
-                        .build()
-
-        );
-
-
-        navigationTabBar.setModels(models);
-        //navigationTabBar.setViewPager(viewPager, 0);
-        navigationTabBar.setBgColor (getResources().getColor( R.color.tabbarbgcolor));
-        navigationTabBar.setInactiveColor(getResources().getColor(R.color.tabbardefaultitemcolor));
-        navigationTabBar.setActiveColor(getResources().getColor(R.color.white));
-
-
-        navigationTabBar.setBehaviorEnabled(false);
-
-        navigationTabBar.setIsSwiped(false);
-
-
-        navigationTabBar.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
 
-//                Utils.consoleLog(MainTabActivity.class,"onPageScrolled");
+                    case R.id.cricket_tab:
 
+                        if(currTag.equals("news")){
+                            return true;
+                        }
+                        selectedFragment = new NewsFragment();
+                        currTag = "news";
+                        break;
+                    case R.id.football_tab:
+                        if(currTag.equals("buzz")){
+                            return true;
+                        }
+                        selectedFragment = new BuzzFragment();
+                        currTag = "buzz";
+                        break;
+                    case R.id.tennis_tab:
+                        if(currTag.equals("live")){
+                            return true;
+                        }
+                        selectedFragment = new LiveFragment();
+                        currTag = "live";
+                        break;
 
-            }
-
-            @Override
-            public void onPageSelected(final int position) {
-                navigationTabBar.getModels().get(position).hideBadge();
-//                Utils.consoleLog(MainTabActivity.class,"onPageSelected");
-//
-//                Config.getSharedInstance().selectedTab = position;
-//                switch (position)
-//                {
-//                    case 0:
-//                        DashboardFragment dashboardFragment = (DashboardFragment)tabsFragmentAdapter.getItem(0);
-//                        dashboardFragment.refreshFragment();
-//                        break;
-//                    case 1:
-//                        LeadsFragment leadsFragment = (LeadsFragment) tabsFragmentAdapter.getItem(1);
-//                        leadsFragment.refreshFragment();
-//                        break;
-//
-//                    case 2:
-//                        RevenueFragment revenueFragment = (RevenueFragment) tabsFragmentAdapter.getItem(2);
-//                        revenueFragment.refreshFragment();
-//                        break;
-//
-//
-//                    case 3:
-//                        TeamActivitiesFragment teamActivitiesFragment = (TeamActivitiesFragment) tabsFragmentAdapter.getItem(3);
-////                        teamActivitiesFragment.refreshFragment();
-//                        break;
-//
-//
-//
-//                    case 4:
-//                        SettingsFragment settingsFragment = (SettingsFragment) tabsFragmentAdapter.getItem(4);
-//                        settingsFragment.refreshFragment();
-//                        break;
-//
-//
-//                }
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(final int state) {
-//                Utils.consoleLog(MainTabActivity.class,"onPageScrollStateChanged");
+                }
+                currFragment = selectedFragment;
 
 
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.content_frame, currFragment,currTag);
+                        transaction.commit();
+                    }
+                });
+                return true;
             }
         });
 
-
+        android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_frame, new NewsFragment(),"cricket");
+        transaction.commit();
+        currTag = "cricket";
 
 
     }
